1afccec88ab7494bd615763f9c31477d60584c98
Kristoffer Jakobsson <kristoffer.tatel.jakobsson@gmail.com>
2023-01-27 00:08:44 +0100

    Damped Quaternion

---
+++ Assets/Scripts/Utility/CommonObjects/DampedQuaternion.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace VirtualDeviants.Utility.CommonObjects
+{
+	public struct DampedQuaternion
+	{
+
+		private Quaternion _velocity;
+		
+		public Quaternion Current { get; private set; }
+		public Quaternion Target { get; set; }
+
+		public Quaternion Tick(float damping)
+		{
+			float deltaTime = Time.deltaTime;
+			return Tick(damping, deltaTime);
+		}
+
+		public Quaternion Tick(float damping, float deltaTime)
+		{
+			
+			return Current;
+		}
+
+	}
+}
